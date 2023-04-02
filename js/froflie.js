d3c4c2a8dad306f0e3fa7fa7bf2e7de6fd376379
Sungbeom <mkonji5466@gmail.com>
2023-04-02 15:45:51 +0900

    20230402 cdy

--- js/froflie.js
+++ js/froflie.js
@@ -1,34 +1,33 @@
 // Profile //
 
-var menu1 = document.querySelector('#menu1');
-var menu2 = document.querySelector('#menu2');
-var menu3 = document.querySelector('#menu3');
-var menu4 = document.querySelector('#menu4');
-var c_box = document.querySelector('.c-box');
-var d_box = document.querySelector('.d-box');
-var v_box = document.querySelector('.v-box');
+const menu1 = document.querySelector("#menu1");
+const menu2 = document.querySelector("#menu2");
+const menu3 = document.querySelector("#menu3");
+const menu4 = document.querySelector("#menu4");
+const c_box = document.querySelector(".c-box");
+const d_box = document.querySelector(".d-box");
+const v_box = document.querySelector(".v-box");
 
+menu1.addEventListener("mouseover", function () {
+  c_box.style.display = "block";
+  d_box.style.display = "block";
+  v_box.style.display = "block";
+});
 
-menu1.addEventListener('mouseover', function(){
-    c_box.style.display = "block";
-    d_box.style.display = "block";
-    v_box.style.display = "block";
-})
+menu2.addEventListener("mouseover", function () {
+  c_box.style.display = "block";
+  d_box.style.display = "none";
+  v_box.style.display = "none";
+});
 
-menu2.addEventListener('mouseover', function(){
-    c_box.style.display = "block";
-    d_box.style.display = "none";
-    v_box.style.display = "none";
-})
+menu3.addEventListener("mouseover", function () {
+  c_box.style.display = "none";
+  d_box.style.display = "block";
+  v_box.style.display = "none";
+});
 
-menu3.addEventListener('mouseover', function(){
-    c_box.style.display = "none";
-    d_box.style.display = "block";
-    v_box.style.display = "none";
-})
-
-menu4.addEventListener('mouseover', function(){
-    c_box.style.display = "none";
-    d_box.style.display = "none";
-    v_box.style.display = "block";
-})
+menu4.addEventListener("mouseover", function () {
+  c_box.style.display = "none";
+  d_box.style.display = "none";
+  v_box.style.display = "block";
+});
